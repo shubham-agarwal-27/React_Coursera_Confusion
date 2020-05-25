24e539758ab2c99cb702699fe31704e3620fa4dc
shubham-agarwal-27 <f20170126@goa.bits-pilani.ac.in>
2020-05-25 13:29:23 +0530

    Changed

--- pre-push_example.sh
+++ pre-push_example.sh
@@ -4,12 +4,12 @@ protected_branch='master'
 current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 if [ $protected_branch = $current_branch ]; then
-    read -p "You're about to push master, is that what you intended? [y|n] " -n 1 -r < /dev/tty
+    read -p "You're about to push master, is that what you intended? [y|n] " -n 3 -r < /dev/tty
     echo
     if echo $REPLY | grep -E '^[Yy]$' > /dev/null; then
         exit 0 # push will execute
     fi
     exit 1 # push will not execute
 else
-    exit 0 # push will execute
+    exit 0 # push will execute this time
 fi
--- test.js
+++ test.js
@@ -1,27 +1,16 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const tty = require('tty');
-
-if (!process.stdin.isTTY) {
-  const { O_RDONLY, O_NOCTTY } = fs.constants;
-  let fd;
-  try {
-    fd = fs.openSync('/dev/tty', O_RDONLY + O_NOCTTY);
-  } catch (error) {
-    console.error('Please push your code in a terminal.');
-    process.exit(1);
-  }
-
-  const stdin = new tty.ReadStream(fd);
-
-  Object.defineProperty(process, 'stdin', {
-    configurable: true,
-    enumerable: true,
-    get: () => stdin,
-  });
-}
-
-cosole.log("Ends the test.js file");
-process.stdin.destroy();
-process.exit(0);
+var readline = require('readline');
+var input = require('fs').createReadStream('/dev/tty');
+var rl = readline.createInterface({
+  input: input
+, output: process.stdout
+//, terminal: false
+});
+rl.question("This is the prompt: ", function (response) {
+  console.log("This is the response:", response);
+  input.close();
+  rl.close();
+  // If I don't close the input explicitly, it stays open.
+  
+});
