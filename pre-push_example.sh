048996435632deca8e5e89bee4e9d4d9bd853940
shubham-agarwal-27 <f20170126@goa.bits-pilani.ac.in>
2020-05-25 16:30:09 +0530

    Changed

--- pre-push_example.sh
+++ pre-push_example.sh
@@ -83,3 +83,21 @@ rl.question('What do you think of Node.js? ', (answer) => {
   console.log(`Thank you for your valuable feedback: ${answer}`);
   rl.close();
 });
+
+
+
+
+
+
+
+
+
+
+
+var stdin = process.openStdin(); 
+require('tty').setRawMode(true);    
+
+stdin.on('keypress', function (chunk, key) {
+  process.stdout.write('Get Chunk: ' + chunk + '\n');
+  if (key && key.ctrl && key.name == 'c') process.exit();
+});
