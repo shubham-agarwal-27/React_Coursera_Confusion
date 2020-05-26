42bb22b6cbf16b4a30dfa88d7ca7ac0b1221ff44
shubham-agarwal-27 <f20170126@goa.bits-pilani.ac.in>
2020-05-26 14:59:13 +0530

    # Please include useful commit message!

--- pre-push_example.sh
+++ pre-push_example.sh
@@ -146,3 +146,90 @@ process.stdin.on('keypress', function(char, key) {
     process.exit()  
   }  
 });  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env node 
+
+
+
+
+import { forceStdinTty } from 'force-stdin-tty';
+ 
+let overwroteStdin = false;
+try {
+  overwroteStdin = forceStdinTty();
+} catch {
+  console.error('Please push your code in a terminal.');
+  process.exit(1);
+}
+ 
+if (overwroteStdin) {
+  process.stdin.destroy();
+}
+
+
+
+// var fs = require('fs');
+// const readline = require('readline');
+
+// console.log(process);
+
+// // fs.writeFileSync('abcd.txt', process, "utf8");
+
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback:`);
+//   console.log("\n");
+//   console.log(`Thank you for your valuable feedback: abcd`);
+
+//   rl.close();
+// });
+
+
+
+
+
+
+// const prompt = require('prompt');
+
+// prompt.start();
+
+// prompt.get(['username', 'email'], function (err, result) {
+//     if (err) { return onErr(err); }
+//     console.log('Command-line input received:');
+//     console.log('  Username: ' + result.username);
+//     console.log('  Email: ' + result.email);
+// });
+
+// function onErr(err) {
+//     console.log(err);
+//     return 1;
+// }
