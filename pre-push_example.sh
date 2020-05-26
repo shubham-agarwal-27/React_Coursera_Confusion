5721365e89c712f97728e361f8fdcb7e9c021080
shubham-agarwal-27 <f20170126@goa.bits-pilani.ac.in>
2020-05-26 13:34:57 +0530

    # Please include useful commit message!

--- pre-push_example.sh
+++ pre-push_example.sh
@@ -127,3 +127,22 @@ process.on('SIGINT', function(){
     console.log(input);
     process.exit();
 });
+
+
+
+
+
+
+
+
+#!/usr/bin/env node
+
+var tty = require('tty');  
+process.stdin.setRawMode(true);  
+process.stdin.resume();  
+ console.log('I am leaving now');  
+process.stdin.on('keypress', function(char, key) {  
+  if (key && key.ctrl && key.name == 'c') {  
+    process.exit()  
+  }  
+});  
